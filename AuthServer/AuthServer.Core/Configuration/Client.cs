2aaa311d1407dc4038ac6b603ad58c68808607f6
Cihat Solak <cihatsolak@hotmail.com>
2021-02-12 12:34:55 +0300

    token service interface added

---
+++ AuthServer/AuthServer.Core/Configuration/Client.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AuthServer.Core.Configuration
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Secret { get; set; }
+        public List<string> Audiences { get; set; }
+    }
+}
