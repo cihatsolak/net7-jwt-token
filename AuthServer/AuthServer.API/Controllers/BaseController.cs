f243b4f996638fc690084d2eefa49f4f623d532f
Cihat Solak <cihatsolak@hotmail.com>
2021-02-20 15:26:00 +0300

    action methods for token have been added.

---
+++ AuthServer/AuthServer.API/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Models;
+
+namespace AuthServer.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        public IActionResult ActionResultInstance<TResponseModel>(ResponseModel<TResponseModel> responseModel) where TResponseModel : class
+        {
+            return new ObjectResult(responseModel)
+            {
+                StatusCode = responseModel.StatusCode
+            };
+        }
+    }
+}
