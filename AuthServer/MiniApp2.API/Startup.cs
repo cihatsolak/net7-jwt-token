d7e5975afd433e9959680161771c5fb5abe9c3a3
Cihat Solak <cihatsolak@hotmail.com>
2021-02-21 17:31:09 +0300

    three api projects added

--- AuthServer/MiniApp2.API/Startup.cs
+++ AuthServer/MiniApp2.API/Startup.cs
@@ -1,13 +1,11 @@
-using AuthServer.API.Middlewares;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 using SharedLibrary.Extensions;
 
-namespace AuthServer.API
+namespace MiniApp2.API
 {
     public class Startup
     {
@@ -18,34 +16,26 @@ namespace AuthServer.API
 
         public IConfiguration Configuration { get; }
 
-        // Bu yöntem çalýþma zamaný tarafýndan çaðrýlýr. Kapsayýcýya hizmet eklemek için bu yöntemi kullanýn.
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSettingsConfiguration(Configuration);
-            services.AddServicesConfiguration(Configuration);
-            services.AddAuthenticationConfiguration(Configuration);
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AuthServer.API", Version = "v1" });
-            });
+            services.AddAuthenticationConfiguration(Configuration);
         }
 
-        // Bu yöntem çalýþma zamaný tarafýndan çaðrýlýr. HTTP istek ardýþýk düzenini yapýlandýrmak için bu yöntemi kullanýn.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AuthServer.API v1"));
             }
 
             app.UseHttpsRedirection();
+
             app.UseRouting();
 
-            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
