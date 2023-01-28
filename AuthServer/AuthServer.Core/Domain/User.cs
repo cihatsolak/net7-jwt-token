8d64046c38d257e5e2802b1d9ffb6c2c3f69cb31
Cihat Solak <cihatsolak@hotmail.com>
2023-01-28 23:05:36 +0300

    Add birth day authorization requirement

--- AuthServer/AuthServer.Core/Domain/User.cs
+++ AuthServer/AuthServer.Core/Domain/User.cs
@@ -1,10 +1,12 @@
 ﻿using AuthServer.Core.Domain.Interfaces;
 using Microsoft.AspNetCore.Identity;
+using System;
 
 namespace AuthServer.Core.Domain
 {
     public class User : IdentityUser<string>, IEntityTable
     {
         public string Country { get; set; }
+        public DateTime BirthDate { get; set; }
     }
 }
